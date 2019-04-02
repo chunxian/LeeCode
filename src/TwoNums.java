4a0215b2e45ec130b510a1d046c54f3559a46cb6
chunxian guo <632442032@qq.com>
2019-04-02 21:46:37 +0800

    hash method

--- src/TwoNums.java
+++ src/TwoNums.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoNums {
 //    给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 //    你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
@@ -8,7 +11,7 @@ public class TwoNums {
     public static void main(String[] args) {
         int[] nums = {3,2,4};
         int target =6;
-        int[] returnArr = new TwoNums().twoSum(nums, target);
+        int[] returnArr = new TwoNums().twoSum1(nums, target);
         for (int value : returnArr){
             System.out.println(value);
         }
@@ -29,5 +32,18 @@ public class TwoNums {
         }
         return returnArr;
 
+    }
+
+    //using hash
+    public int[] twoSum1(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0 ; i < nums.length; i ++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp)) {
+               return new int[]{map.get(temp), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
     }
 }
